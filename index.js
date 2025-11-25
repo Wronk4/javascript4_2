6089ed8a5383aa4051c5c31ddaa77dbb3f6d4059
Kacper Wrona <66514795+Wronk4@users.noreply.github.com>
2025-11-25 11:25:34 +0100

    4_2.1

---
+++ index.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+let categories = ['funnyJoke', 'lameJoke'];
+let funnyJoke = [
+  {
+    'joke': 'Dlaczego komputer poszedł do lekarza?',
+    'response': 'Bo złapał wirusa!'
+  },
+  {
+    'joke': 'Dlaczego komputer nie może być głodny?',
+    'response': 'Bo ma pełen dysk!'
+  },
+  {
+    'joke': 'Co mówi jeden bit do drugiego?',
+    'response': '„Trzymaj się, zaraz się przestawiamy!”'
+  }
+];
+let lameJoke = [
+  {
+    'joke': 'Dlaczego programiści preferują noc?',
+    'response': 'Bo w nocy jest mniej bugów do łapania!'
+  },
+  {
+    'joke': 'Jak nazywa się bardzo szybki programista?',
+    'response': 'Błyskawiczny kompilator!'
+  }
+];
+
+app.get('/jokebook/categories', (req, res) => {
+  res.json(categories);
+});
+
+app.get('/jokebook/joke/:category', (req, res) => {
+  const category = req.params.category;
+  const jokeLists = {
+    'funnyJoke': funnyJoke,
+    'lameJoke': lameJoke
+  };
+
+  if (categories.includes(category)) {
+    const list = jokeLists[category];
+    const randomJoke = list[Math.floor(Math.random() * list.length)];
+    res.json(randomJoke);
+  } else {
+    res.json({ 'error': `no jokes for category ${category}` });
+  }
+});
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
