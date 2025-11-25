f44b4559aea244b26f29f8d0fc6ac14c039385fb
Kacper Wrona <66514795+Wronk4@users.noreply.github.com>
2025-11-25 11:26:05 +0100

    4_2.2

--- index.js
+++ index.js
@@ -50,7 +50,25 @@ app.get('/jokebook/joke/:category', (req, res) => {
   }
 });
 
-
+app.post('/jokebook/joke/:category', (req, res) => {
+  const category = req.params.category;
+  const { joke, response } = req.body || {};
+  const jokeLists = {
+    'funnyJoke': funnyJoke,
+    'lameJoke': lameJoke
+  };
+console.log(req.body)
+  if (categories.includes(category)) {
+    if (joke && response) {
+      jokeLists[category].push({ joke, response });
+      res.json("success");
+    } else {
+      res.status(400).json({ error: 'invalid body format' });
+    }
+  } else {
+    res.json({ 'error': `no jokes for category ${category}` });
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
